ee787a6bf970faa44e5305bac3726488dd239cf9
Dax Harris <matteovh@gmail.com>
2023-10-17 19:02:48 -0400

    wip book-contrib

--- app_types/book.py
+++ app_types/book.py
@@ -1,6 +1,32 @@
 from psycopg import Connection
 from util.orm import Record, ORM
 from datetime import datetime
+from typing import Literal
+
+class ContributorRecord(Record):
+    def __init__(self, db: Connection, table: str, orm: ORM, type: Literal["author", "publisher", "editor"], id: int, first_name: str, last_name_or_company: str, *args) -> None:
+        super().__init__(db, table, orm, *args)
+        self.type = type
+        self.id = id
+        self.first_name = first_name
+        self.last_name_or_company = last_name_or_company
+
+    @property
+    def name(self):
+        if self.type == "publisher":
+            return self.last_name_or_company.title()
+        return (self.first_name + " " + self.last_name_or_company).title()
+    
+    @property
+    def books(self):
+        match self.type:
+            case "author":
+                relation_table = "books_authors"
+            case "editor":
+                relation_table = "books_editors"
+            case "publisher":
+                relation_table = "books_publishers"
+        
 
 
 class BookRecord(Record):
@@ -57,5 +83,26 @@ class BookRecord(Record):
     def genres(self) -> dict[int, str]:
         cursor = self.db.execute("SELECT * FROM genres AS root WHERE id IN (SELECT genre_id FROM books_genres WHERE book_id = %(id)s)", {"id": self.id})
         results = {r[0]:r[1] for r in cursor.fetchall()}
+        cursor.close()
+        return results
+    
+    @property
+    def authors(self) -> list[ContributorRecord]:
+        cursor = self.db.execute("SELECT * FROM contributors AS root WHERE id IN (SELECT contributor_id FROM books_authors WHERE book_id = %(id)s)", {"id": self.id})
+        results = [ContributorRecord(self.db, "contributors", self.orm, "author", *r) for r in cursor.fetchall()]
+        cursor.close()
+        return results
+
+    @property
+    def editors(self) -> list[ContributorRecord]:
+        cursor = self.db.execute("SELECT * FROM contributors AS root WHERE id IN (SELECT contributor_id FROM books_editors WHERE book_id = %(id)s)", {"id": self.id})
+        results = [ContributorRecord(self.db, "contributors", self.orm, "editor", *r) for r in cursor.fetchall()]
+        cursor.close()
+        return results
+    
+    @property
+    def publishers(self) -> list[ContributorRecord]:
+        cursor = self.db.execute("SELECT * FROM contributors AS root WHERE id IN (SELECT contributor_id FROM books_publishers WHERE book_id = %(id)s)", {"id": self.id})
+        results = [ContributorRecord(self.db, "contributors", self.orm, "publisher", *r) for r in cursor.fetchall()]
         cursor.close()
         return results
