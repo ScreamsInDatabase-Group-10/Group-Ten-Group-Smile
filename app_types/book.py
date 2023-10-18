d18add0ff53477e4171fb0dddb30c5d9fe0ba516
iTecAI <matteovh@gmail.com>
2023-10-18 14:41:37 -0400

    Add additional methods to book

--- app_types/book.py
+++ app_types/book.py
@@ -1,10 +1,21 @@
 from psycopg import Connection
 from util.orm import Record, ORM
 from datetime import datetime
-from typing import Literal
+from typing import Literal, Optional
+
 
 class ContributorRecord(Record):
-    def __init__(self, db: Connection, table: str, orm: ORM, type: Literal["author", "publisher", "editor"], id: int, first_name: str, last_name_or_company: str, *args) -> None:
+    def __init__(
+        self,
+        db: Connection,
+        table: str,
+        orm: ORM,
+        type: Literal["author", "publisher", "editor"],
+        id: int,
+        first_name: str,
+        last_name_or_company: str,
+        *args,
+    ) -> None:
         super().__init__(db, table, orm, *args)
         self.type = type
         self.id = id
@@ -12,13 +23,13 @@ class ContributorRecord(Record):
         self.last_name_or_company = last_name_or_company
 
     @property
-    def name(self):
+    def name(self) -> str:
         if self.type == "publisher":
             return self.last_name_or_company.title()
         return (self.first_name + " " + self.last_name_or_company).title()
-    
+
     @property
-    def books(self):
+    def books(self) -> list["BookRecord"]:
         match self.type:
             case "author":
                 relation_table = "books_authors"
@@ -26,7 +37,33 @@ class ContributorRecord(Record):
                 relation_table = "books_editors"
             case "publisher":
                 relation_table = "books_publishers"
-        
+
+        cursor = self.db.execute(
+            "SELECT * FROM books AS root WHERE id IN (SELECT book_id FROM {rel} WHERE contributor_id = %(id)s)".format(
+                rel=relation_table
+            ),
+            {"id": self.id},
+        )
+        results = [BookRecord(self.db, "books", self.orm, *r) for r in cursor.fetchall()]
+        cursor.close()
+        return results
+    
+    def save(self):
+        self.db.execute(
+            "UPDATE "
+            + self.table
+            + " SET name_first = %s, name_last_company = %s WHERE id = %s",
+            (
+                self.first_name,
+                self.last_name_or_company,
+                self.id,
+            ),
+        )
+        self.db.commit()
+
+    def delete(self):
+        self.db.execute("DELETE FROM " + self.table + " WHERE id = %s", (self.id,))
+        self.db.commit()
 
 
 class BookRecord(Record):
@@ -73,36 +110,60 @@ class BookRecord(Record):
     # Return an {id:name} mapping of audiences
     @property
     def audiences(self) -> dict[int, str]:
-        cursor = self.db.execute("SELECT * FROM audiences AS root WHERE id IN (SELECT audience_id FROM books_audiences WHERE book_id = %(id)s)", {"id": self.id})
-        results = {r[0]:r[1] for r in cursor.fetchall()}
+        cursor = self.db.execute(
+            "SELECT * FROM audiences AS root WHERE id IN (SELECT audience_id FROM books_audiences WHERE book_id = %(id)s)",
+            {"id": self.id},
+        )
+        results = {r[0]: r[1] for r in cursor.fetchall()}
         cursor.close()
         return results
-    
+
     # Return an {id:name} mapping of genres
     @property
     def genres(self) -> dict[int, str]:
-        cursor = self.db.execute("SELECT * FROM genres AS root WHERE id IN (SELECT genre_id FROM books_genres WHERE book_id = %(id)s)", {"id": self.id})
-        results = {r[0]:r[1] for r in cursor.fetchall()}
+        cursor = self.db.execute(
+            "SELECT * FROM genres AS root WHERE id IN (SELECT genre_id FROM books_genres WHERE book_id = %(id)s)",
+            {"id": self.id},
+        )
+        results = {r[0]: r[1] for r in cursor.fetchall()}
         cursor.close()
         return results
-    
+
     @property
     def authors(self) -> list[ContributorRecord]:
-        cursor = self.db.execute("SELECT * FROM contributors AS root WHERE id IN (SELECT contributor_id FROM books_authors WHERE book_id = %(id)s)", {"id": self.id})
-        results = [ContributorRecord(self.db, "contributors", self.orm, "author", *r) for r in cursor.fetchall()]
+        cursor = self.db.execute(
+            "SELECT * FROM contributors AS root WHERE id IN (SELECT contributor_id FROM books_authors WHERE book_id = %(id)s)",
+            {"id": self.id},
+        )
+        results = [
+            ContributorRecord(self.db, "contributors", self.orm, "author", *r)
+            for r in cursor.fetchall()
+        ]
         cursor.close()
         return results
 
     @property
     def editors(self) -> list[ContributorRecord]:
-        cursor = self.db.execute("SELECT * FROM contributors AS root WHERE id IN (SELECT contributor_id FROM books_editors WHERE book_id = %(id)s)", {"id": self.id})
-        results = [ContributorRecord(self.db, "contributors", self.orm, "editor", *r) for r in cursor.fetchall()]
+        cursor = self.db.execute(
+            "SELECT * FROM contributors AS root WHERE id IN (SELECT contributor_id FROM books_editors WHERE book_id = %(id)s)",
+            {"id": self.id},
+        )
+        results = [
+            ContributorRecord(self.db, "contributors", self.orm, "editor", *r)
+            for r in cursor.fetchall()
+        ]
         cursor.close()
         return results
-    
+
     @property
     def publishers(self) -> list[ContributorRecord]:
-        cursor = self.db.execute("SELECT * FROM contributors AS root WHERE id IN (SELECT contributor_id FROM books_publishers WHERE book_id = %(id)s)", {"id": self.id})
-        results = [ContributorRecord(self.db, "contributors", self.orm, "publisher", *r) for r in cursor.fetchall()]
+        cursor = self.db.execute(
+            "SELECT * FROM contributors AS root WHERE id IN (SELECT contributor_id FROM books_publishers WHERE book_id = %(id)s)",
+            {"id": self.id},
+        )
+        results = [
+            ContributorRecord(self.db, "contributors", self.orm, "publisher", *r)
+            for r in cursor.fetchall()
+        ]
         cursor.close()
         return results
