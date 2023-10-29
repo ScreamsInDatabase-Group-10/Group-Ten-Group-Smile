059be8be5360433cbd7bd417000f9d3d5b9c32a6
maddymakesgames <madelinemakesgames@gmail.com>
2023-10-29 12:37:45 -0400

    display user list in panel

--- app_types/user.py
+++ app_types/user.py
@@ -1,8 +1,38 @@
 from psycopg import Connection
-from util.orm import Record, ORM
+from util.orm import (
+    PaginationParams,
+    Record,
+    ORM,
+    SearchCondition,
+    SearchResult,
+    search_internal,
+)
 from datetime import datetime
 import time
-from typing import Union
+from typing import Optional, Union
+
+USER_FMT = """
+SELECT
+        users.id as id,
+        users.name_first as name_first,
+        users.name_last as name_last,
+        users.email as email,
+        users.password as password,
+        users.creation_dt as creation_dt,
+        users.access_dt as access_dt
+    FROM {table}
+    {conditions}
+    {order}
+    {offset}
+    {limit}
+"""
+
+USER_FMT_COUNT = """
+SELECT
+        COUNT(users.id)
+    from {table}
+    {conditions}
+"""
 
 
 class UserRecord(Record):
@@ -28,7 +58,7 @@ class UserRecord(Record):
         self.name_last = name_last
         self.email = email
         self.password = password
-    
+
     def save(self):
         self.db.execute(
             "UPDATE "
@@ -41,7 +71,7 @@ class UserRecord(Record):
                 self.name_last,
                 self.email,
                 self.password,
-                self.id
+                self.id,
             ),
         )
         self.db.commit()
@@ -49,25 +79,95 @@ class UserRecord(Record):
     def delete(self):
         self.db.execute("DELETE FROM " + self.table + " WHERE id = %s", (self.id,))
         self.db.commit()
-    
+
     # Create a user, handle ID generation and times automatically
     @classmethod
-    def create(cls, orm: ORM, name_first: str, name_last: str, email: str, password: str) -> Union["UserRecord", None]:
+    def create(
+        cls, orm: ORM, name_first: str, name_last: str, email: str, password: str
+    ) -> Union["UserRecord", None]:
         next_id = orm.next_available_id("users")
         creation = datetime.fromtimestamp(time.time())
         access = datetime.fromtimestamp(time.time())
 
         try:
-            orm.db.execute("INSERT INTO users (id, creation_dt, access_dt, name_first, name_last, email, password) VALUES (%s, %s, %s, %s, %s, %s, %s)", (
+            orm.db.execute(
+                "INSERT INTO users (id, creation_dt, access_dt, name_first, name_last, email, password) VALUES (%s, %s, %s, %s, %s, %s, %s)",
+                (next_id, creation, access, name_first, name_last, email, password),
+            )
+            orm.db.commit()
+            return UserRecord(
+                orm.db,
+                "users",
+                orm,
                 next_id,
                 creation,
                 access,
                 name_first,
                 name_last,
                 email,
-                password
-            ))
-            orm.db.commit()
-            return UserRecord(orm.db, "users", orm, next_id, creation, access, name_first, name_last, email, password)
+                password,
+            )
         except:
             return None
+
+    @classmethod
+    def _from_search(
+        cls,
+        db: Connection,
+        table: str,
+        orm: ORM,
+        id: int,
+        name_first: str,
+        name_last: str,
+        email: str,
+        creation_dt: datetime,
+        access_dt: datetime,
+        password: str,
+    ):
+        return UserRecord(
+            db,
+            table,
+            orm,
+            id,
+            creation_dt,
+            access_dt,
+            name_first,
+            name_last,
+            email,
+            password,
+        )
+
+    @classmethod
+    def search(
+        self,
+        orm: ORM,
+        pagination: PaginationParams,
+        id: Optional[str] = None,
+        name_first: Optional[str] = None,
+        name_last: Optional[str] = None,
+        email: Optional[str] = None,
+    ) -> SearchResult:
+        fields = []
+
+        if id != None:
+            fields.append(SearchCondition("id = %s", [id]))
+        if name_first != None:
+            fields.append(SearchCondition("name_first ilike %s", [name_first]))
+        if name_last != None:
+            fields.append(SearchCondition("name_last ilike %s", [name_last]))
+        if email != None:
+            fields.append(SearchCondition("email ilike %s", [email]))
+
+        results = search_internal(
+            orm,
+            "users",
+            UserRecord._from_search,
+            fields,
+            pagination.get("order") if pagination else None,
+            pagination.get("offset") if pagination else None,
+            pagination.get("limit") if pagination else None,
+            format_query=USER_FMT,
+            # format_count_query=USER_FMT_COUNT,
+        )
+
+        return results
--- screens/panels/users_panel.py
+++ screens/panels/users_panel.py
@@ -1,14 +1,62 @@
 from textual.app import ComposeResult
-from textual.widgets import Placeholder
-from textual.containers import Container
+from textual.widgets import Button, Input, ListView, Label, ListItem
+from textual.containers import Horizontal, Container, VerticalScroll
+from app_types.user import UserRecord
 from util import ContextWidget
+from util.pagination import PaginatedTable
 
 
 class UsersPanel(ContextWidget):
     def compose(self) -> ComposeResult:
         yield Container(
-            Placeholder("Search Bar", id="user-search-section", classes="panel-sections user-search"),
-            Placeholder("User Results", id="user-results-section", classes="panel-sections user-results"),
+            Horizontal(
+                Input(value="", placeholder="Search Users", id="search-main"),
+                Button("Search", id="btn-search"),
+                Button("Advanced", id="btn-advanced"),
+                id="user-search-section",
+                classes="panel-sections users-search",
+            ),
+            PaginatedTable(
+                UserRecord,
+                [
+                    {
+                        "key": "id",
+                        "name": "User Id",
+                        "render": lambda id: str(id),
+                        "sort_by": "id",
+                    },
+                    {
+                        "key": "name_first",
+                        "name": "First Name",
+                        "render": lambda name: name,
+                        "sort_by": "name_first",
+                    },
+                    {
+                        "key": "name_last",
+                        "name": "Last Name",
+                        "render": lambda name: name,
+                        "sort_by": "name_last",
+                    },
+                    {
+                        "key": "creation_dt",
+                        "name": "Creation Date",
+                        "render": lambda dt: dt,
+                    },
+                    {
+                        "key": "email",
+                        "name": "Email",
+                        "render": lambda email: email,
+                    },
+                ],
+                id="user-results-section",
+                classes="panel-selections user-results",
+                initial_params={},
+                initial_pagination={
+                    "limit": 25,
+                    "offset": 0,
+                    "order": [["name_first", "ASC"], ["name_last", "ASC"]],
+                },
+            ),
             classes="panel users",
             id="app-panel-users",
         )
