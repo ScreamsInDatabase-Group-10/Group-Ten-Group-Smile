7ca4f20b5a0edee68d7691f68ee0106aa887c60b
The-ai123 <sans34678@gmail.com>
2023-10-30 18:43:22 -0400

    Saving changes to self_panel.py to pull from main

--- screens/panels/self_panel.py
+++ screens/panels/self_panel.py
@@ -1,10 +1,18 @@
 from textual.app import ComposeResult
-from textual.widgets import Placeholder, Label, Static
-from textual.containers import Container
+from textual.widgets import Placeholder, Label, Static, DataTable, ContentSwitcher, Button
+from textual.containers import Container, Horizontal
 from util import ContextWidget
 
 
 
+class ConnectionsPanel(Static):
+    def compose(self) -> ComposeResult:
+        with Horizontal(id="buttons"):
+            yield Button("Following", id="following-table")
+            yield Button("Followers", id="follower-table")
+        with ContentSwitcher(initial="following-table"):
+            yield DataTable(id="following-table")
+            yield DataTable(id="follower-table")
 
 class SelfPanel(ContextWidget):
     def compose(self) -> ComposeResult:
@@ -13,7 +21,8 @@ class SelfPanel(ContextWidget):
             Static("Email: " + user.email + " Name: " + user.name_first + " " + user.name_last + "\nDate Created: " + user.creation_dt.strftime('%b %d %Y')
                    , id="user-info-section", classes="panel-sections user-info"),
             Placeholder("Collections", id="collections-section", classes="panel-sections collections"),
-            Placeholder("Connections", id="connections-section", classes="panel-sections connections"),
+            ConnectionsPanel(),
             classes="panel self",
             id="app-panel-self",
         )
+
