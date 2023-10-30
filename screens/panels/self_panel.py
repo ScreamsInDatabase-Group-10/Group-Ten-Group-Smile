4dd1d19a44e7f43a2d9c858cc461d43bdce718c7
The-ai123 <sans34678@gmail.com>
2023-10-30 19:23:04 -0400

    Created follower and following tab and put in a placeholder table

--- screens/panels/self_panel.py
+++ screens/panels/self_panel.py
@@ -1,18 +1,105 @@
 from textual.app import ComposeResult
-from textual.widgets import Placeholder, Label, Static, DataTable, ContentSwitcher, Button
+from textual.widgets import Placeholder, Label, Static, DataTable, ContentSwitcher, Button, TabbedContent, TabPane
 from textual.containers import Container, Horizontal
 from util import ContextWidget
+from util.pagination import PaginatedTable
+from app_types.user import UserRecord
+
 
 
 
 class ConnectionsPanel(Static):
     def compose(self) -> ComposeResult:
-        with Horizontal(id="buttons"):
-            yield Button("Following", id="following-table")
-            yield Button("Followers", id="follower-table")
-        with ContentSwitcher(initial="following-table"):
-            yield DataTable(id="following-table")
-            yield DataTable(id="follower-table")
+        with TabbedContent(id="account_connections"):
+            with TabPane("Following", id="following"):
+                yield PaginatedTable(
+                    UserRecord,
+                    [
+                        {
+                            "key": "id",
+                            "name": "User Id",
+                            "render": lambda id: str(id),
+                            "sort_by": "id",
+                        },
+                        {
+                            "key": "name_first",
+                            "name": "First Name",
+                            "render": lambda name: name,
+                            "sort_by": "name_first",
+                        },
+                        {
+                            "key": "name_last",
+                            "name": "Last Name",
+                            "render": lambda name: name,
+                            "sort_by": "name_last",
+                        },
+                        {
+                            "key": "creation_dt",
+                            "name": "Creation Date",
+                            "render": lambda dt: dt.strftime("%b %d, %Y"),
+                        },
+                        {
+                            "key": "email",
+                            "name": "Email",
+                            "render": lambda email: email,
+                        },
+                    ],
+                    id="user-results-section",
+                    classes="panel-selections user-results",
+                    initial_params={},
+                    initial_pagination={
+                        "limit": 25,
+                        "offset": 0,
+                        "order": [["name_first", "ASC"], ["name_last", "ASC"]],
+                    },
+                )
+            with TabPane("Followers", id="followers"):
+                yield PaginatedTable(
+                    UserRecord,
+                    [
+                        {
+                            "key": "id",
+                            "name": "User Id",
+                            "render": lambda id: str(id),
+                            "sort_by": "id",
+                        },
+                        {
+                            "key": "name_first",
+                            "name": "First Name",
+                            "render": lambda name: name,
+                            "sort_by": "name_first",
+                        },
+                        {
+                            "key": "name_last",
+                            "name": "Last Name",
+                            "render": lambda name: name,
+                            "sort_by": "name_last",
+                        },
+                        {
+                            "key": "creation_dt",
+                            "name": "Creation Date",
+                            "render": lambda dt: dt.strftime("%b %d, %Y"),
+                        },
+                        {
+                            "key": "email",
+                            "name": "Email",
+                            "render": lambda email: email,
+                        },
+                    ],
+                    id="user-results-section",
+                    classes="panel-selections user-results",
+                    initial_params={},
+                    initial_pagination={
+                        "limit": 25,
+                        "offset": 0,
+                        "order": [["name_first", "ASC"], ["name_last", "ASC"]],
+                    },
+                )
+
+
+
+
+
 
 class SelfPanel(ContextWidget):
     def compose(self) -> ComposeResult:
