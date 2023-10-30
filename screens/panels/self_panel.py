1be274c49372c8926a86a0608707b7aadfc9f8c2
GabeFrahm <gabefrahm@gmail.com>
2023-10-30 00:29:37 -0400

    implemented viewing collections in the self panel

--- screens/panels/self_panel.py
+++ screens/panels/self_panel.py
@@ -1,10 +1,7 @@
 from textual.app import ComposeResult
-from textual.widgets import Placeholder, Label, Static
+from textual.widgets import Placeholder, Label, Static, ListView, ListItem
 from textual.containers import Container
 from util import ContextWidget
-
-
-
 
 class SelfPanel(ContextWidget):
     def compose(self) -> ComposeResult:
@@ -12,7 +9,9 @@ class SelfPanel(ContextWidget):
         yield Container(
             Static("Email: " + user.email + " Name: " + user.name_first + " " + user.name_last + "\nDate Created: " + user.creation_dt.strftime('%b %d %Y')
                    , id="user-info-section", classes="panel-sections user-info"),
-            Placeholder("Collections", id="collections-section", classes="panel-sections collections"),
+            ListView(
+                *[ListItem(Static(c.name)) for c in user.collections()]
+            ),
             Placeholder("Connections", id="connections-section", classes="panel-sections connections"),
             classes="panel self",
             id="app-panel-self",
