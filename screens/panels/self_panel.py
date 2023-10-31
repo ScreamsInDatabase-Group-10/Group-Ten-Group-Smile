a1b104b37b4473867ef0bcaecee1c54a538f2330
GabeFrahm <gabefrahm@gmail.com>
2023-10-31 19:52:33 -0400

    created a Collection type for front-end

--- screens/panels/self_panel.py
+++ screens/panels/self_panel.py
@@ -1,7 +1,41 @@
 from textual.app import ComposeResult
-from textual.widgets import Placeholder, Label, Static, ListView, ListItem
+from textual.widgets import Placeholder, Label, Static, ListView, ListItem, Button
 from textual.containers import Container
+from app_types.user import CollectionRecord, UserRecord
 from util import ContextWidget
+
+def CollectionContainer(user: UserRecord) -> Container:
+    # TODO: make container fill all the way down
+    if(len(user.collections()) == 0):
+        return Container(
+                Static("Collections"),
+                Static("No user collections! Make your first one below!")
+            )
+    return Container(
+                Static("Collections"),
+                ListView(
+                    *[ListItem(Collection(c)) for c in user.collections()]
+                )
+            )
+
+class Collection(Static):
+    def __init__(
+            self,
+            collection: CollectionRecord,
+            name: str | None = None,
+            id: str | None = None,
+            classes: str | None = None,
+            disabled: bool = False,
+
+    ):
+        super().__init__(
+            name=name, id=id, classes=classes, disabled=disabled
+        )
+        self.collection = collection
+
+    def compose(self) -> ComposeResult:
+        yield Static(self.collection.name)
+        yield Button("Edit", id=f"collection-button-{self.collection.id}")
 
 class SelfPanel(ContextWidget):
     def compose(self) -> ComposeResult:
@@ -9,11 +43,7 @@ class SelfPanel(ContextWidget):
         yield Container(
             Static("Email: " + user.email + " Name: " + user.name_first + " " + user.name_last + "\nDate Created: " + user.creation_dt.strftime('%b %d %Y')
                    , id="user-info-section", classes="panel-sections user-info"),
-            Container(
-                ListView(
-                    *[ListItem(Static(c.name)) for c in user.collections()]
-                )
-            ),
+            CollectionContainer(user),
             Placeholder("Connections", id="connections-section", classes="panel-sections connections"),
             classes="panel self",
             id="app-panel-self",
