3877fe2c49d4ea08afc669978ddab1905a7dc142
maddymakesgames <madelinemakesgames@gmail.com>
2023-11-03 18:20:10 -0400

    format

--- screens/panels/self_panel.py
+++ screens/panels/self_panel.py
@@ -1,7 +1,18 @@
 from textual import on
 from textual.app import ComposeResult
 from textual.widget import Widget
-from textual.widgets import Placeholder, Label, Static, TabbedContent, TabPane, DataTable, ListView, ListItem, Button, Input
+from textual.widgets import (
+    Placeholder,
+    Label,
+    Static,
+    TabbedContent,
+    TabPane,
+    DataTable,
+    ListView,
+    ListItem,
+    Button,
+    Input,
+)
 from textual.containers import Container, Grid
 from textual import work, on
 from textual.reactive import reactive
@@ -9,6 +20,8 @@ from app_types.book import BookRecord
 from app_types.user import CollectionRecord, UserRecord
 from util.widget import ContextModal
 from util import ContextWidget
+
+
 class ConnectionsPanel(ContextWidget):
     followers: reactive[list[UserRecord]] = reactive([])
     following: reactive[list[UserRecord]] = reactive([])
@@ -16,16 +29,20 @@ class ConnectionsPanel(ContextWidget):
     @work(thread=True)
     async def load_followers(self):
         self.followers = self.context.logged_in.followers
-    
+
     @work(thread=True)
     async def load_following(self):
         self.following = self.context.logged_in.following
-        
+
     def on_mount(self):
         self.load_followers()
         self.load_following()
-        self.query_one("#table-followers", expect_type=DataTable).add_columns("First Name", "Last Name", "Email")
-        self.query_one("#table-following", expect_type=DataTable).add_columns("First Name", "Last Name", "Email", "Murder Button")
+        self.query_one("#table-followers", expect_type=DataTable).add_columns(
+            "First Name", "Last Name", "Email"
+        )
+        self.query_one("#table-following", expect_type=DataTable).add_columns(
+            "First Name", "Last Name", "Email", "Murder Button"
+        )
 
     def watch_followers(self, old, new: list[UserRecord]):
         table = self.query_one("#table-followers", expect_type=DataTable)
@@ -35,7 +52,9 @@ class ConnectionsPanel(ContextWidget):
     def watch_following(self, old, new):
         table = self.query_one("#table-following", expect_type=DataTable)
         table.clear()
-        table.add_rows([(i.name_first, i.name_last, i.email, "[b]KILL[/b]") for i in new])
+        table.add_rows(
+            [(i.name_first, i.name_last, i.email, "[b]KILL[/b]") for i in new]
+        )
 
     def compose(self) -> ComposeResult:
         with TabbedContent():
@@ -48,9 +67,13 @@ class ConnectionsPanel(ContextWidget):
     def handle_kill_click(self, event: DataTable.CellHighlighted):
         if event.value == "[b]KILL[/b]":
             record: UserRecord = self.following[event.coordinate.row]
-            self.context.db.execute("DELETE FROM users_following WHERE user_id = %s AND following_id = %s", [self.context.logged_in.id, record.id])
+            self.context.db.execute(
+                "DELETE FROM users_following WHERE user_id = %s AND following_id = %s",
+                [self.context.logged_in.id, record.id],
+            )
             del self.following[event.coordinate.row]
             self.watch_following([], self.following)
+
 
 class CollectionContainer(ContextWidget):
     def compose(self) -> ComposeResult:
@@ -58,16 +81,24 @@ class CollectionContainer(ContextWidget):
             with TabPane("Collections"):
                 yield Button("Create Collection", id="create-collection-button")
                 yield ListView(
-                    *[ListItem(Collection(c), classes="list-item") for c in self.context.logged_in.collections()],
-                    id="collection-list"
+                    *[
+                        ListItem(Collection(c), classes="list-item")
+                        for c in self.context.logged_in.collections()
+                    ],
+                    id="collection-list",
                 )
-    
+
     @on(Button.Pressed, "#create-collection-button")
     def create_collection_button(self):
         self.query_one("#collection-list", expect_type=ListView).mount(
-            ListItem(Collection(CollectionRecord.create(self.context.orm, "New Collection", self.context.logged_in)))
+            ListItem(
+                Collection(
+                    CollectionRecord.create(
+                        self.context.orm, "New Collection", self.context.logged_in
+                    )
+                )
+            )
         )
-
 
 
 class CollectionEditModal(ContextModal):
@@ -80,14 +111,22 @@ class CollectionEditModal(ContextModal):
     ) -> None:
         super().__init__(name, id, classes)
         self.collection = collection
-        self.collection.books = self.collection._init_books() # refresh book list
+        self.collection.books = self.collection._init_books()  # refresh book list
         self.newName = collection.name
 
     def compose(self) -> ComposeResult:
         with Grid(id="collection-edit-modal-container"):
-            yield Static(f"[b]Edit Collection: \"{self.collection.name}\"[/b]", id="edit-title")
-            yield Static(f"[b]Book Count:[/b] {self.collection.book_count}", classes="collection-info")
-            yield Static(f"[b]Page Count:[/b] {self.collection.page_count}", classes="collection-info")
+            yield Static(
+                f'[b]Edit Collection: "{self.collection.name}"[/b]', id="edit-title"
+            )
+            yield Static(
+                f"[b]Book Count:[/b] {self.collection.book_count}",
+                classes="collection-info",
+            )
+            yield Static(
+                f"[b]Page Count:[/b] {self.collection.page_count}",
+                classes="collection-info",
+            )
             yield ListItem(
                 Static("[b]Name[/b]"), classes="input-label", id="label-name"
             )
@@ -99,8 +138,11 @@ class CollectionEditModal(ContextModal):
                 name="name",
             )
             yield ListView(
-                *[ListItem(CollectionBook(b, self.collection), classes="list-item") for b in self.collection.books],
-                id="book-list"
+                *[
+                    ListItem(CollectionBook(b, self.collection), classes="list-item")
+                    for b in self.collection.books
+                ],
+                id="book-list",
             )
             yield Button("Save", id="save-button")
             yield Button("Delete", id="delete-button")
@@ -120,20 +162,20 @@ class CollectionEditModal(ContextModal):
     @on(Button.Pressed, "#cancel-button")
     def on_close(self):
         self.dismiss(None)
-    
+
     def on_input_changed(self, event: Input.Changed):
         self.newName = event.input.value
 
 
 class CollectionBook(Static):
     def __init__(
-            self,
-            book: BookRecord,
-            collection: CollectionRecord,
-            name: str | None = None,
-            id: str | None = None,
-            classes: str | None = None,
-            disabled: bool = False,
+        self,
+        book: BookRecord,
+        collection: CollectionRecord,
+        name: str | None = None,
+        id: str | None = None,
+        classes: str | None = None,
+        disabled: bool = False,
     ):
         super().__init__(name=name, id=id, classes=classes, disabled=disabled)
         self.book = book
@@ -146,10 +188,11 @@ class CollectionBook(Static):
 
     @on(Button.Pressed, "#toggle-button")
     def on_remove(self):
-        if (self.removed == False):
+        if self.removed == False:
             # TODO: Change button title to red strikethrough
             self.query_one("#title", expect_type=Static).update(
-                f"[s]{self.book.title}[/s]")
+                f"[s]{self.book.title}[/s]"
+            )
             self.query_one("#toggle-button", expect_type=Button).label = "Add"
             self.removed = True
             self.collection.remove_book(self.book)
@@ -162,12 +205,12 @@ class CollectionBook(Static):
 
 class Collection(Static):
     def __init__(
-            self,
-            collection: CollectionRecord,
-            name: str | None = None,
-            id: str | None = None,
-            classes: str | None = None,
-            disabled: bool = False
+        self,
+        collection: CollectionRecord,
+        name: str | None = None,
+        id: str | None = None,
+        classes: str | None = None,
+        disabled: bool = False,
     ):
         super().__init__(name=name, id=id, classes=classes, disabled=disabled)
         self.collection = collection
@@ -177,31 +220,44 @@ class Collection(Static):
         yield Button("View/Edit", id="collection-button")
 
     def collection_update(self, collection: CollectionRecord | None) -> None:
-        if collection == None: return
+        if collection == None:
+            return
         if collection.deleted:
             self.remove()
         else:
             self.collection = collection
             self.query_one("#collection-name", expect_type=Static).update(
                 self.collection.name
-                )
+            )
 
     @on(Button.Pressed, "#collection-button")
     def on_edit(self):
-        self.app.push_screen(CollectionEditModal(self.collection), self.collection_update)
+        self.app.push_screen(
+            CollectionEditModal(self.collection), self.collection_update
+        )
 
 
 class SelfPanel(ContextWidget):
     def compose(self) -> ComposeResult:
         user = self.context.logged_in
         yield Container(
-            Static("Email: " + user.email + " Name: " + user.name_first + " " + user.name_last + "\nDate Created: " +
-                   user.creation_dt.strftime('%b %d %Y'), id="user-info-section", classes="panel-sections user-info"),
+            Static(
+                "Email: "
+                + user.email
+                + " Name: "
+                + user.name_first
+                + " "
+                + user.name_last
+                + "\nDate Created: "
+                + user.creation_dt.strftime("%b %d %Y"),
+                id="user-info-section",
+                classes="panel-sections user-info",
+            ),
             CollectionContainer(id="collections-section"),
             ConnectionsPanel(id="connections-section"),
             classes="panel self",
             id="app-panel-self",
         )
-    
-    #@on(Button.Pressed, "create-button")
-    #def on_create_collection():
+
+    # @on(Button.Pressed, "create-button")
+    # def on_create_collection():
